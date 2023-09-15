ee311bfb6819810932fa9886bf5bd9b1235e4c79
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-09-15 14:43:34 -0400

    passes first test

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,23 +16,38 @@ spicy_foods = [
     },
 ]
 
+
 def get_names(spicy_foods):
-    pass
+    list_name = [d.get("name", "") for d in spicy_foods]
+    print(list_name)
+    return list_name
+
 
 def get_spiciest_foods(spicy_foods):
+    # Define a function get_spiciest_foods() that takes a list of spicy_foods and returns a list of dictionaries where the heat level of the food is greater than 5.
     pass
+
 
 def print_spicy_foods(spicy_foods):
+    # Define a function print_spicy_foods() that takes a list of spicy_foods and output to the terminal each spicy food in the following format using print(): Buffalo Wings (American) | Heat Level: 🌶🌶🌶.
     pass
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    # Define a function get_spicy_food_by_cuisine() that takes a list of spicy_foods and a string representing a cuisine, and returns a single dictionary for the spicy food whose cuisine matches the cuisine being passed to the method.
     pass
+
 
 def print_spiciest_foods(spicy_foods):
+    # Define a function print_spiciest_foods() that takes a list of spicy_foods and outputs to the terminal ONLY the spicy foods that have a heat level greater than 5, in the following format: Buffalo Wings (American) | Heat Level: 🌶🌶🌶.
     pass
+
 
 def get_average_heat_level(spicy_foods):
+    # Define a function average_heat_level() that takes a list of spicy_foods and returns an integer representing the average heat level of all the spicy foods in the array.
     pass
 
+
 def create_spicy_food(spicy_foods, spicy_food):
+    # Define a function create_spicy_food() that takes a list of spicy_foods and a new spicy_food and returns the original list with the new spicy_food added.
     pass
